5e87be702db37e4a14872ac93606fbf3d69af788
StevenZeng <790494072@qq.com>
2017-12-28 10:36:57 +0800

    nothing

--- apps/catalogue/urls/catalogue.py
+++ apps/catalogue/urls/catalogue.py
@@ -5,6 +5,6 @@ urlpatterns = patterns('',
    url(r'^(?P<category>[a-z-0-9/]+)/$', CategoryList.as_view(), name="category"),
    #url(r'^$', ProductList.as_view(), name="all_products")
    url(r'$', ProductList.as_view(), name="all_products")
-                       
+
    #url(r'$', test, name="all_products")
 )
